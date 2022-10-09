0a2f2244f4eac6c6b897d4da9cd2b5b17683ead7
Kindstep <kindstep@arcada.fi>
2022-10-09 13:09:03 +0300

    latest

--- app.py
+++ app.py
@@ -14,12 +14,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DB_URL')
 db.init_app(app)
 
 # datamodell = tabell i postgresql
-class User2(db.Model):
+class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
+    order = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
 
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -28,34 +36,36 @@ def index():
 
         return {
         'method': request.method,
-        'msg': 'Webhook works on Rahti!',
+        'msg': 'Received all orders',
         'env': os.environ.get('ENV_VAR', 'Cannot find variable ENV_VAR') 
         }
 
     if request.method == 'POST':
         body = request.get_json()
         return {
-            'msg': 'You posted successfully',
+            'msg': 'You posted an order successfully',
             'request_body': body
         }
 
-@app.route("/users", methods=['GET', 'POST'])
-def users():
+@app.route("/order", methods=['GET', 'POST'])
+def orders():
     if request.method =='GET':
-        users = [],
-        for user in User2.query.all():
-            users.append({
-                'id': user.id,
-                'email': user.email,
-                'updated_at': user.updated_at
+        orders = [],
+        for order in Order.query.all():
+            orders.append({
+                'id': order.id,
+                'order': order.order,
+                'updated_at': order.updated_at
             })
 
     if request.method == 'POST':
         body =  request.get_json()
-        new_user = User2(email=body['email'])
-        db.session.add(new_user)
-        db.session.commit()
-        return {'msg': 'User created', 'id':new_user.id}
+        new_order = Order(order=body)
+        db.session.add(new_order)
+        db.session.commit(new_order)
+
+        return {'msg': 'Order created', 'id':new_order.id}
+        
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
